dfa162fd844f5c3a556c8878c03fcde563a17315
unknown <pravesh256@gmail.com>
2021-07-17 15:02:59 +0530

    New Test cases

--- src/test/java/CAIRObjectRepository/CAIRDashboardPage.java
+++ src/test/java/CAIRObjectRepository/CAIRDashboardPage.java
@@ -10,7 +10,7 @@ public class CAIRDashboardPage {
 	WebDriver driver;
 
 	@FindBy(id = "twDashboard")
-	WebElement DashBoard;
+	WebElement DashBoardButton;
 	@FindBy(id = "twColrelate")
 	WebElement Articles;
 	@FindBy(id = "twUsers")
@@ -24,44 +24,82 @@ public class CAIRDashboardPage {
 	@FindBy(id = "idSaveSearch")
 	WebElement Save;
 
+	@FindBy(xpath = "//*[@id=\"widget_dashboard_60-1\"]/div[2]/div/span")
+	WebElement TOPLANGUAGES;
+	@FindBy(xpath = "//*[@id=\"widget_dashboard_3-1\"]/div[2]/div[1]/span")
+	WebElement TOPIMAGES;
+	@FindBy(xpath = "//*[@id=\"widget_dashboard_4-1\"]/div[2]/div/span")
+	WebElement TOPURLS;
+
+	@FindBy(xpath = "//*[@id=\"widget_dashboard_86-1\"]/div[2]/div[1]/span")
+	WebElement HOURLYACTIVITYCOUNT;
+	@FindBy(xpath = "//*[@id=\"widget_dashboard_9-1\"]/div[2]/div/span")
+	WebElement ORG;
+
 	public CAIRDashboardPage(WebDriver driver) {
 
 		this.driver = driver;
-        PageFactory.initElements(driver, this);
+		PageFactory.initElements(driver, this);
 
 	}
 
-	public WebElement DashBoard() {
-		return DashBoard;
-		
+	public WebElement DashBoardButton() {
+		return DashBoardButton;
+
 	}
-	
+
 	public WebElement Articles() {
 		return Articles;
-		
+
 	}
+
 	public WebElement Author() {
 		return Author;
-		
+
 	}
+
 	public WebElement Map() {
 		return Map;
-		
+
 	}
+
 	public WebElement Media() {
 		return Media;
-		
+
 	}
+
 	public WebElement Report() {
 		return Report;
-		
+
 	}
+
 	public WebElement Save() {
 		return Save;
-		
+
 	}
-	
-		
+
+	public WebElement TOPLANGUAGES() {
+		return TOPLANGUAGES;
+
 	}
-	
-	
+
+	public WebElement TOPIMAGES() {
+		return TOPIMAGES;
+
+	}
+
+	public WebElement TOPURLS() {
+		return TOPURLS;
+	}
+
+	public WebElement HOURLYACTIVITYCOUNT() {
+		return HOURLYACTIVITYCOUNT;
+
+	}
+
+	public WebElement ORG() {
+		return ORG;
+
+	}
+
+}
